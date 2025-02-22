e6cd065e62c97c478f7012391c51e7527dad990c
Sounak <sounaksaha333@gmail.com>
2025-02-22 23:35:19 +0530

    0.2 => JSX rules

--- src/component/HelloWorld.js
+++ src/component/HelloWorld.js
@@ -1,12 +1,28 @@
 import React from "react";
 
 const HelloWorld = () => {
-    const myElement = <div><h1> Hello World!</h1></div>
-    return myElement
+    // const myElement = <div><h1> Hello World!</h1></div>
+    // return myElement
     // return React
     //     .createElement('div', null, 
     //         React.createElement('h1', null, 'Hello World!')
     //     )
+
+    // rules of JSX
+
+    const name = "Soumitra"
+    const age = 15 + 3
+    const handleClick = () => alert("Button clicked!")
+
+    return (
+        <div>
+            <h1 className="title">Hello World</h1>
+            <h2>Description</h2>
+            <p>This is a paragraph</p>
+            <p> Name: {name}, Age: {age} </p>
+            <button onClick={handleClick}>Click</button>
+        </div>
+    )
 }
 
 export default HelloWorld;
