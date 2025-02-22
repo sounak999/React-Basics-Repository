6afe2edaf71df9234ddd7b70aa1bec23e5754ce3
Sounak <sounaksaha333@gmail.com>
2025-02-22 21:04:55 +0530

    0.1 => functional component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Welcome from './component/Welcome';
 
 function App() {
   return (
     <div className="App">
-		<h1>Hello World</h1>
+		<Welcome />
     </div>
   );
 }
